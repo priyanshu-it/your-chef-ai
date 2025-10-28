618c9515917896ec9eca94a0e200964e499dd86a
Priyanshu <sepriyanshu21@gmail.com>
2025-10-29 00:10:45 +0530

    src/ai/genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,7 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { genkit } from 'genkit';
-import { googleAI } from '@genkit-ai/google-genai';
-
-dotenv.config({ path: path.resolve(__dirname, '../lib/.env') });
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [ googleAI({ apiKey: process.env.ICONS_DEMO, }), ],
+  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.ICONS_DEMO})],
   model: 'googleai/gemini-2.5-flash',
 });
