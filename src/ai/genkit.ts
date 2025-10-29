c1d98210e5fc339e1f83d552ce93b45d9389d240
Priyanshu <sepriyanshu21@gmail.com>
2025-10-29 12:14:41 +0530

    src/ai/genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { ICONS_DEMO} from '../lib/.env';
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.ICONS_DEMO})],
