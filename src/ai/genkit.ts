2211ccb826d7b0400a3239af552e7d632b84fd1f
Priyanshu <sepriyanshu21@gmail.com>
2025-10-29 12:19:02 +0530

    src/ai/genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.ICONS_DEMO})],
+  plugins: [googleAI({ apiKey: process.env.ICONS_DEMO })],
   model: 'googleai/gemini-2.5-flash',
 });
